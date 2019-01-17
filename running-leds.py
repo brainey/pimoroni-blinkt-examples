560f1bb0727922667fcb9550e07d778a67a23d83
Bill Brainey <brainey@sonic.net>
2019-01-16 23:24:30 -0800

    first commit

---
+++ running-leds.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from blinkt import set_pixel, set_brightness, show, clear
+import time
+
+set_brightness(0.2)
+
+while True:
+    for i in range(8):
+        clear()
+        set_pixel(i, 255, 0, 0)
+        show()
+        time.sleep(0.05)
